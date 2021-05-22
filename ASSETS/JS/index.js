5c51fb8b398dc156e6a6b8d622891026bd474ed7
Luis Zunino <zuninoluis01@gmail.com>
2021-05-22 14:16:46 -0300

    index.js
    se agrega el condicional

--- ASSETS/JS/index.js
+++ ASSETS/JS/index.js
@@ -1,5 +1,17 @@
 const nombreUsuario = prompt("Por favor ingresar su nombre:");
-const lugar = prompt(
-  `¡Bienvenido ${nombreUsuario} al auomatizador de viajes! Elige el tipo de destino: Playa/Campo/Ciudad`
+const tiempo = prompt(
+  `¡Bienvenido ${nombreUsuario} al auomatizador de viajes! Elige el tiempo que deseas hospedarte`
 ).toUpperCase();
 console.log("El usuario a ingresado la información")
+
+
+if (tiempo > 0 && tiempo < 5) {
+    alert(`Muy bien ${nombreUsuario} has elegido hospedarte ${tiempo} días`);
+
+} else if (tiempo > 5 && tiempo <= 7) {
+    alert(`Muy bien ${nombreUsuario} has elegido hospedarte ${tiempo} días`);
+
+} else {
+    alert(`Muy bien ${nombreUsuario} has elegido hospedarte ${tiempo} días`);
+
+}
