c0be7a6e95f32177c4c372a3a00a174ad21a9e00
Luis Zunino <zuninoluis01@gmail.com>
2021-06-02 14:34:34 -0300

    package.json
    se incluye un ciclo for, se creean los archivos de estilos css y scss, se instala  sass y nodemon

--- ASSETS/JS/index.js
+++ ASSETS/JS/index.js
@@ -15,3 +15,49 @@ if (tiempo > 0 && tiempo < 5) {
     alert(`Muy bien ${nombreUsuario} has elegido hospedarte ${tiempo} días`);
 
 }
+
+//Listar Departamentos
+let depas = [
+    "--Departamento--",
+    "Artigas",
+    "Canelones",
+    "Cerrolargo",
+    "Colonia",
+    "Durazno",
+    "Flores",
+    "Florida",
+    "Lavalleja",
+    "Maldonado",
+    "Montevideo",
+    "Paysandu",
+    "Río Negro",
+    "Rivera",
+    "Rocha",
+    "Salto",
+    "San José",
+    "Soriano",
+    "Tacuarembo",
+    "Treinta y Tres",
+  ];
+  let depa = document.getElementById("depa");
+  
+  for (let i in depas) {
+    depa.innerHTML += `<option id="${i}">${depas[i]}</option>`;
+  }
+  //Tipos de lugares
+  let tipo = [
+    "--Elige una opción--",
+    "Espacios Verdes",
+    "Playas",
+    "Aguas Termales",
+    "Ciudades",
+  ];
+  let tipos = document.getElementById("tipos");
+  
+  for(let i in tipo){
+    tipos.innerHTML += `<option id="${i}">${tipo[i]}</option>`;
+  
+  }
+  
+  
+  
