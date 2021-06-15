8a919febf8d677080b2ab9c738adca5a19579140
Luis Zunino <zuninoluis01@gmail.com>
2021-06-15 16:21:37 -0300

    mapa.js
    se modifica la posicion inicial del mapa y ademas lo que muestra cada marcador

--- ASSETS/JS/mapa.js
+++ ASSETS/JS/mapa.js
@@ -1,9 +1,28 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+/*
+https://www.danielprimo.io/blog/leaflet-una-alternativa-gratis-a-google-maps
+*/
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+var map = L.map("map").setView([-32.70693340531241, -55.98485369606557], 7);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+for (let viaje of viajes) {
+  const texto = buscado.value.toLowerCase();
+  let destino = viaje.nombre.toLowerCase();
+  L.marker([viaje.cordX, viaje.cordY])
+    .addTo(map)
+    .bindPopup(
+      `<img src="ASSETS/IMG/DESTINOS/unnamed(${viaje.img}).jpg">
+        <div> 
+          <h1>${viaje.nombre}</h1>
+          <span>
+              <p>Departamento: ${viaje.departamento}</p>
+              <p>${viaje.info}</p> 
+          </span>
+        </div>`
+    )
     .openPopup();
+}
