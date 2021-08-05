523b258e2e02a2f057919ad041fd8f36d9ac203f
Luis Zunino <72168167+Luis-zunino@users.noreply.github.com>
2021-08-04 21:40:43 -0300

    index.js
    se crean funciones para poder visualizar un solo elemento a la vez y la funcion para regresar al total de resultados

--- ASSETS/JS/index.js
+++ ASSETS/JS/index.js
@@ -1,4 +1,10 @@
-/*Listar Departamentos*/
+//selectores
+document.querySelector("#buscador").addEventListener("keyup", buscador);
+let depa = document.getElementById("depa");
+let tipos = document.getElementById("tipos");
+let map = L.map("map").setView([-32.70693340531241, -55.98485369606557], 7);
+//Arrays
+let viajes = [];
 let depas = [
   "--Departamento--",
   "Artigas",
@@ -21,14 +27,6 @@ let depas = [
   "Tacuarembo",
   "Treinta y Tres",
 ];
-/*eligo donde lo voy a imprimir*/
-let depa = document.getElementById("depa");
-
-for (let i = 0; i < depas.length; i++) {
-  depa.innerHTML += `<option id="${i}"><span>${depas[i]}</span></option>`;
-}
-
-/*Tipos de lugares*/
 let tipo = [
   "--Elige una opción--",
   "Espacios Verdes",
@@ -37,7 +35,9 @@ let tipo = [
   "Ciudades",
 ];
 
-let tipos = document.getElementById("tipos");
+for (let i = 0; i < depas.length; i++) {
+  depa.innerHTML += `<option id="${i}"><span>${depas[i]}</span></option>`;
+}
 for (let i = 0; i < tipo.length; i++) {
   tipos.innerHTML += `<option id="${i}">${tipo[i]}</option>`;
 }
@@ -48,8 +48,39 @@ $(document).ready(function () {
     $(".keep").toggleClass("width");
   });
 });
+//Simulacion de HTMLs
+document.getElementById("aside").addEventListener("click", (e) => {
+  let listaDestinos = $(".cajaResultados");
+  let mapa = $(".cajaMapaInteractivo");
+  let agenda = $("#agendados");
+  let r = e.target.classList;
 
-let viajes = [];
+  if (r.contains("amarillo") || r.contains("yellow")) {
+    removerClases();
+
+    listaDestinos.addClass("mostrar");
+    mapa.addClass("ocultar");
+    agenda.addClass("ocultar");
+  } else if (r.contains("rojo") || r.contains("red")) {
+    removerClases();
+
+    listaDestinos.addClass("ocultar");
+    mapa.addClass("mostrar");
+    agenda.addClass("ocultar");
+  } else if (r.contains("verde") || r.contains("green")) {
+    removerClases();
+
+    listaDestinos.addClass("ocultar");
+    mapa.addClass("ocultar");
+    agenda.addClass("mostrar");
+  }
+
+  function removerClases() {
+    listaDestinos.removeClass("mostrar");
+    mapa.removeClass("mostrar");
+    agenda.removeClass("mostrar");
+  }
+});
 
 //AJAX
 $.ajax({
@@ -59,11 +90,10 @@ $.ajax({
   success: function (result, status, jqXHR) {
     viajes = result;
     cargarCardViajes(viajes);
+    cargarMapa(viajes);
   },
   error: function (jqXHR, status, error) {},
 });
-
-document.querySelector("#buscador").addEventListener("keyup", buscador);
 
 function buscador() {
   const texto = document.querySelector("#buscador").value;
@@ -72,8 +102,9 @@ function buscador() {
     return viaje.nombre.toUpperCase().indexOf(texto.toUpperCase()) !== -1;
   });
   cargarCardViajes(nombresFiltrados);
+  cargarMapa(nombresFiltrados);
 }
-
+//Recorro el array que me trae el AJAX, le doy forma y lo imprimo
 function cargarCardViajes(viajes) {
   bubbleSort(viajes);
 
@@ -90,8 +121,9 @@ function cargarCardViajes(viajes) {
             <h4>${elemento.info}</h4> 
         </span>
       </div>
-      <a href="VIEWS/mapa.html" id="${elemento.id}" class="agendar"><i class="far fa-calendar-check"></i><i class="far fa-calendar-times"></i></a>
-      <p><i class="fas fa-map-marked-alt"></i></p>
+      <a class="agendar"><i  class="far fa-calendar-check"></i><i class="far fa-calendar-times"></i></a>
+      <a><i class="fas fa-map-marked-alt"></i></a>
+      <a id="${elemento.id}">Expandir</a>
 
     </li>
   `;
@@ -116,66 +148,66 @@ function bubbleSort(a) {
   }
 }
 
-//mapa
-let map = L.map("map").setView([-32.70693340531241, -55.98485369606557], 7);
-
-//L.marker([elemento.cordX, elemento.cordY]).addTo(map).bindPopup().openPopup();
-
+//Mapa
 function cargarMapa(viajes) {
-  viajes.map((elemento) => {
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {}).addTo(
-      map
-    );
-    L.marker([elemento.cordX, elemento.cordY])
+  // let map = L.map("map").setView([-32.70693340531241, -55.98485369606557], 7);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+  viajes.map((pin) => {
+    L.marker([pin.cordX, pin.cordY])
       .addTo(map)
       .bindPopup(
-        `<img src="../ASSETS/IMG/DESTINOS/unnamed(${elemento.img}).jpg">
-           <div>
-             <h1>${elemento.nombre}</h1>
-             <span>
-                 <p>Departamento: ${elemento.departamento}</p>
-                 <p>${elemento.info}</p>
-             </span>
-           </div>`
+        `<img src="../ASSETS/IMG/DESTINOS/unnamed(${pin.img}).jpg">
+      <div>
+      <h1>${pin.nombre}</h1>
+      <span>
+      <p>Departamento: ${pin.departamento}</p>
+      <p>${pin.info}</p>
+      </span>
+      </div>`
       )
       .openPopup();
   });
 }
-cargarMapa(viajes)
 
-//Simulacion de HTMLs
-document.getElementById("aside").addEventListener("click", (e) => {
-  let listaDestinos = $(".cajaResultados");
-  let mapa = $(".cajaMapaInteractivo");
-  let agenda = $("#agendados");
-  let r = e.target.classList;
-  if (r.contains("amarillo") || r.contains("yellow")) {
+//Generador de Cards individuales de los destinos
+document
+  .getElementById("viajes")
+  .addEventListener("click", (e) => creadorIndividuales(e));
 
-    removerClases();
-    listaDestinos.addClass("mostrar");
-    mapa.addClass("ocultar");
-    agenda.addClass("ocultar");
+function creadorIndividuales(e) {
+  let id = e.target.getAttribute("id");
+  let x = e.target.parentElement;
+  let h3 = x.getElementsByTagName("h3")[0].innerText;
+  let departamento = x.getElementsByTagName("h4")[0].innerText;
+  let info = x.getElementsByTagName("h4")[1].innerText;
+  if (id) {
+    document.getElementById("viajes").innerHTML = "";
 
-  } else if (r.contains("rojo") || r.contains("red")) {
-
-    removerClases();
-
-    listaDestinos.addClass("ocultar");
-    mapa.addClass("mostrar");
-    agenda.addClass("ocultar");
-    
-  } else if (r.contains("verde") || r.contains("green")) {
-    removerClases();
-
-    listaDestinos.addClass("ocultar");
-    mapa.addClass("ocultar");
-    agenda.addClass("mostrar");
-
+    document.getElementById("viajes").innerHTML = `
+  <li>
+      
+      <img src="ASSETS/IMG/DESTINOS/unnamed(${id}).jpg">
+      <div> 
+        <h3>${h3}</h3>
+        <span class="contViaje">
+          <h4>${departamento}</h4>
+          <h4>${info}</h4> 
+        </span>
+      </div>
+      <a class="agendar"><i class="far fa-calendar-check"></i><i class="far fa-calendar-times"></i></a>
+      <a><i class="fas fa-map-marked-alt"></i></a>
+  </li>
+  `;
   }
 
-  function removerClases() {
-    listaDestinos.removeClass("mostrar");
-    mapa.removeClass("mostrar");
-    agenda.removeClass("mostrar");
-  }
-});
+}
+
+document.getElementById("volver").addEventListener("click", volverResultados);
+function volverResultados() {
+  // let volver = document.getElementById("viajes");
+  document.getElementById("viajes").innerHTML = "";
+  cargarCardViajes(viajes);
+}
